75c63e210ead7253cd0467412d9364994e614fbf
Eddy Erkel <eddy.erkel@gmail.com>
2018-10-25 21:16:04 +0200

    Version 0.4

--- NulleX_NAV_status.sh
+++ NulleX_NAV_status.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 #
 # Creator: Eddy Erkel
-# Version: v0.3
-# Date:    October 24, 2018
+# Version: v0.4
+# Date:    October 25, 2018
 #
 # This script can be used to check you NulleX NAV Masternode status
-# and send an report via email
-# 
-# For more info on NulleX please go to https://nullex.io/
+# This script will:
+# - Verify NulleX masternode status
+# - Verify NulleX block count
+# - Verify NulleX balance at explorer.nullex.io
+# - Verify NulleX installer version
+# - Verify NulleX wallet version
+# - Verify NulleX process ID
+# - Verify number of running NulleX processes
+# - Verify NulleX masternode server disk space
+# - Verify NulleX masternode port connection
+# - Send an email when an warning or error has been raised
+#
+#
+# For more info about NulleX please go to https://nullex.io/
 #
 #
 # This script uses SSMTP to send email:
@@ -22,7 +33,7 @@
 #   0 * * * * /path/to/nullex/script/NulleX_NAV_status.sh
 #
 #
-# Verify functions can be enabled and disabled at the bottom of this script
+# Verify functions can be enabled, disabled and reordered at the bottom of this script
 #
 ####################################################################################################
 
@@ -38,6 +49,7 @@ installfilegithub=https://raw.githubusercontent.com/NLXionaire/nullex-nav-instal
 installfilelocal=~/nullex/nullex-nav-installer.sh   # Local NulleX NAV installation file which was used during set-up
 nullexcli=/usr/local/bin/NulleX/nullex-cli          # nullex-cli path
 pidfile=~/.nullexqt/NulleXd.pid                     # NulleX daemon process id file
+masternodeport=43879								# Port used by NulleX masternode
 processes=1                                         # Expected number of running nullexd processes
 diskspace_alert=90									# set alert level 90% is default
 # diskspace_excludelist="/dev/hdd1|/dev/hdc5"		# Exclude list of unwanted disk space monitoring, use "|" to separate the partitions. Not enabled by default.
@@ -58,6 +70,7 @@ date_time="`date +%Y-%m-%d\ %H:%M:%S`"              # Set date variable
 ssmtp=/usr/sbin/ssmtp                               # ssmtp path
 warnings=0                                          # Set warnings to 0
 errors=0                                            # Set errros to 0
+checks=0											# Set checks to 0
 
 
 ###################################################################################################
@@ -65,7 +78,7 @@ errors=0                                            # Set errros to 0
 ###################################################################################################
 f_disphead ()
 {
-    len="70"
+    len="76"
     string="$1"
     strlen=${#string}                               # ${#string} expands to the length of $string
     n_fill=$(( (len - $strlen - 2) / 2 ))
@@ -158,10 +171,31 @@ if ! [ -e $pidfile ] ; then f_disperr "File not found: $pidfile" ; exit; fi
 
 
 ###################################################################################################
+# Verify masternode status
+###################################################################################################
+f_verify_masternode_status () {
+	let checks+=1
+    f_disphead "Verify NulleX masternode status"
+    $nullexcli masternode status | grep message | sed -e "s/^  \"message\": //" 2>&1 | tee -a $script_mail
+    masternodestatus=$($nullexcli masternode status | grep status | sed -e "s/^  \"status\": //" -e "s/,//")
+
+    # masternodestatus=1                                  # Unhash for script testing
+    
+    if [ "$masternodestatus" == "4" ]; then
+            f_dispnorm "NulleX masternode is running."
+    else
+            f_disperr "NulleX masternode is off-line."
+    fi
+
+    f_dispfoot
+}
+
+
+###################################################################################################
 # Verify block count
 ###################################################################################################
-f_verify_block_count ()
-{
+f_verify_block_count () {
+	let checks+=1
     f_disphead  "Verify NulleX block count"
 
     # Get block count at explorer.nullex.io
@@ -190,10 +224,50 @@ f_verify_block_count ()
 
 
 ###################################################################################################
-# Verify installer verion
+# Verify balance
 ###################################################################################################
-f_verify_installer_version ()
-{
+f_verify_balance () {
+	let checks+=1
+    # Create old file for testing:  touch -t YYYYMMDDhhmm.ss <filename>
+    f_disphead "Verify NulleX balance at explorer.nullex.io"
+    hash=$($nullexcli masternode status | grep "  \"addr\"" | sed -e "s/^  \"addr\": \"//" -e "s/\",//")
+
+    newbalance=$(wget -qO- https://explorer.nullex.io/ext/getbalance/$hash | sed 's/\..*//')
+    f_dispnorm "Checking balance online for $hash"
+    if [ -f $script_bal ]; then
+        oldbalance=$(cat $script_bal)
+        datebalance=$(echo $(stat -c %y $script_bal) | sed 's/\..*//')
+        balancechange=$(echo $(( $(date +%s) - $(stat -L --format %Y "$script_bal") > ($balanceinterval*60) )))
+        f_dispnorm "Previous balance: $oldbalance"
+        f_dispnorm "Current balance : $newbalance"
+        
+    # newbalance=1                                        # Unhash for script testing          
+    # balancechange=1                                     # Unhash for script testing          
+        
+        if [ $newbalance -gt $oldbalance ]; then
+            f_dispnorm "NulleX rewards have been added since previous check at $datebalance."
+            echo "$newbalance" > $script_bal
+        else
+            if [ "$balancechange" -eq "1" ]; then
+                f_dispwarn "NulleX balance change is taking longer than expected. Unchanged since $datebalance."
+            else
+                f_dispnorm "NulleX balance unchanged since previous check at $datebalance."
+            fi
+        fi
+    else
+        f_dispnorm "Current balance : $newbalance"
+            echo "$newbalance" > $script_bal
+    fi
+
+    f_dispfoot
+}
+
+
+###################################################################################################
+# Verify installer version
+###################################################################################################
+f_verify_installer_version () {
+	let checks+=1
     f_disphead "Verify NulleX installer version"
 
     installerversion=$(wget -qO- $installfilegithub | grep '# Version:' | sed -e "s/^# Version: v//")
@@ -217,8 +291,8 @@ f_verify_installer_version ()
 ###################################################################################################
 # Verify wallet version
 ###################################################################################################
-f_verify_wallet_version ()
-{
+f_verify_wallet_version () {
+	let checks+=1
     f_disphead "Verify NulleX wallet version"
     latestwalletversion=$(wget -qO- $installfilegithub | grep '^WALLET_VERSION=\"' | sed -e "s/^WALLET_VERSION=\"//" -e "s/\"//")
     f_dispnorm "Github wallet version: $latestwalletversion"
@@ -239,31 +313,10 @@ f_verify_wallet_version ()
 
 
 ###################################################################################################
-# Verify masternode status
-###################################################################################################
-f_verify_masternode_status ()
-{
-    f_disphead "Verify NulleX masternode status"
-    $nullexcli masternode status | grep message | sed -e "s/^  \"message\": //"
-    masternodestatus=$($nullexcli masternode status | grep status | sed -e "s/^  \"status\": //" -e "s/,//")
-
-    # masternodestatus=1                                  # Unhash for script testing
-    
-    if [ "$masternodestatus" == "4" ]; then
-            f_dispnorm "NulleX masternode status is OK."
-    else
-            f_disperr "NulleX masternode status is NOT OK!"
-    fi
-
-    f_dispfoot
-}
-
-
-###################################################################################################
 # Verify process ID
 ###################################################################################################
-f_verify_process_id ()
-{
+f_verify_process_id () {
+	let checks+=1
     f_disphead "Verify NulleX process ID"
 
     # pidfile=./test                                      # Unhash for script testing
@@ -282,8 +335,8 @@ f_verify_process_id ()
 ###################################################################################################
 # Verify number of running Nullex processes
 ###################################################################################################
-f_verify_nullex_processes ()
-    {
+f_verify_nullex_processes () {
+	let checks+=1
     f_disphead "Verify number of running NulleX processes"
     processcount=$(ps -ef | grep nullexd | grep -v grep | wc -l)
     ps -ef | egrep 'PID|nullexd' | grep -v grep
@@ -302,49 +355,10 @@ f_verify_nullex_processes ()
 
 
 ###################################################################################################
-# Verify balance
-###################################################################################################
-f_verify_balance ()
-{
-    # Create old file for testing:  touch -t YYYYMMDDhhmm.ss <filename>
-    f_disphead "Verify NulleX balance at explorer.nullex.io"
-    hash=$($nullexcli masternode status | grep "  \"addr\"" | sed -e "s/^  \"addr\": \"//" -e "s/\",//")
-
-    newbalance=$(wget -qO- https://explorer.nullex.io/ext/getbalance/$hash | sed 's/\..*//')
-    f_dispnorm "Checking balance online for $hash"
-    if [ -f $script_bal ]; then
-        oldbalance=$(cat $script_bal)
-        datebalance=$(echo $(stat -c %y $script_bal) | sed 's/\..*//')
-        balancechange=$(echo $(( $(date +%s) - $(stat -L --format %Y "$script_bal") > ($balanceinterval*60) )))
-            f_dispnorm "Previous balance: $oldbalance"
-        f_dispnorm "Current balance : $newbalance"
-        
-    # newbalance=1                                        # Unhash for script testing          
-    # balancechange=1                                     # Unhash for script testing          
-        
-        if [ $newbalance -gt $oldbalance ]; then
-            f_dispnorm "NulleX rewards have been added since last check."
-            echo "$newbalance" > $script_bal
-        else
-            if [ "$balancechange" -eq "1" ]; then
-                f_dispwarn "NulleX balance change is taking longer than expected. Unchanged since $datebalance."
-            else
-                f_dispnorm "NulleX balance unchanged since check at $datebalance."
-            fi
-        fi
-    else
-        f_dispnorm "Current balance : $newbalance"
-            echo "$newbalance" > $script_bal
-    fi
-
-    f_dispfoot
-}
-
-
-###################################################################################################
-# Verify disk usage
+# Verify NulleX masternode server disk space
 ###################################################################################################
 f_check_diskspace () {
+	let checks+=1
 	while read output;
 		do
 		  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1)
@@ -357,7 +371,8 @@ f_check_diskspace () {
 }
 
 f_verify_diskspace () {
-    f_disphead "Verify Masternode server disk space"
+	let checks+=1
+    f_disphead "Verify NulleX masternode server disk space"
 	if [ "$diskspace_excludelist" != "" ] ; then
 		df -H | grep -vE "^Filesystem|tmpfs|cdrom|${diskspace_excludelist}" | awk '{print $5 " " $6}' | f_check_diskspace
 		local disk_warnings=$?
@@ -375,16 +390,48 @@ f_verify_diskspace () {
 
 
 ###################################################################################################
+# Verify NulleX masternode port connection
+###################################################################################################
+f_check_port () {
+	let checks+=1
+	f_disphead "Verify NulleX masternode port connection"
+	/bin/netstat -l | egrep -w "$masternodeport|Local Address" >> $script_mail
+	/bin/netstat -l | grep -w "Local Address"
+	/bin/netstat -l | grep -w "$masternodeport"
+	if [ "$?" -ne 0 ]; then
+		f_dispwarn "NulleX masternode is not listening on port $masternodeport."
+	else
+		f_dispnorm "NulleX masternode is listening on port $masternodeport."
+	fi
+	echo "" 2>&1 | tee -a $script_mail
+	publicip=$(/usr/bin/wget -q -O - checkip.dyn.com|sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+	#publicip=$(/usr/bin/curl -s ident.me)
+	f_dispnorm "NulleX masternode external IP-address: $publicip"
+	/bin/nc -z -v -w2 $publicip $masternodeport 2>&1 | tee -a $script_mail
+	if [ "$?" -ne 0 ]; then
+		f_disperr "Connection to $publicip on port $masternodeport failed."
+	else
+		f_dispnorm "Connection to $publicip on port $masternodeport succeeded."
+	fi
+
+	f_dispfoot
+}
+
+
+###################################################################################################
 # Display summary
 ###################################################################################################
 f_display_summary ()
 {
     f_disphead "Summary"
+	f_dispnorm "Checks  : $checks"
     f_dispnorm "Warnings: $warnings"
     f_dispnorm "Errors  : $errors"
-
+	
+	echo -e "\e[92m----------------------------------------------------------------------------\e[0m"
     f_dispfoot
-    }
+
+}
 
     
 ###################################################################################################
@@ -408,21 +455,22 @@ f_send_email ()
     #	ssmtp $mailto -vvv < $script_mail	# Verbose ssmtp output
     fi
 
-    f_dispfoot
+    f_dispfoot 
 }
 
 
 ###################################################################################################
 # Enable, disable and change verification order.
 ###################################################################################################
+f_verify_masternode_status
 f_verify_block_count
+f_verify_balance
 f_verify_installer_version
 f_verify_wallet_version
-f_verify_masternode_status
 f_verify_process_id
 f_verify_nullex_processes
-f_verify_balance
 f_verify_diskspace
+f_check_port
 f_display_summary
 f_write_logfile
 f_send_email
--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 # NulleX_NAV_Masternode_status_report
 #
 # Creator: Eddy Erkel
-# Version: v0.3
-# Date:    October 24, 2018
+# Version: v0.4
+# Date:    October 25, 2018
 #
 # This script can be used to check you NulleX NAV Masternode status
-# and send an report via email
-# 
-# For more info on NulleX please go to https://nullex.io/
+# This script will:
+# - Verify NulleX masternode status
+# - Verify NulleX block count
+# - Verify NulleX balance at explorer.nullex.io
+# - Verify NulleX installer version
+# - Verify NulleX wallet version
+# - Verify NulleX process ID
+# - Verify number of running NulleX processes
+# - Verify NulleX masternode server disk space
+# - Verify NulleX masternode port connection
+# - Send an email when an warning or error has been raised
+#
+#
+# For more info about NulleX please go to https://nullex.io/
 #
 #
 # This script uses SSMTP to send email:
@@ -23,7 +34,7 @@
 #   0 * * * * /path/to/nullex/script/NulleX_NAV_status.sh
 #
 #
-# Verify functions can be enabled and disabled at the bottom of this script
+# Verify functions can be enabled, disabled and reordered at the bottom of this script
 #
 ####################################################################################################
 
@@ -32,45 +43,56 @@ Example output to the script:
 
 Subject: NulleX NAV status report
 
-Tue Oct 23 22:00:01 CEST 2018
+Thu Oct 25 20:50:19 CEST 2018
 
---------------------- Verify NulleX block count ----------------------
-Latest online block: 132700
-Current local block: 132700
+--------------------- Verify NulleX masternode status ----------------------
+"Masternode successfully started"
+NulleX masternode is running.
+
+------------------------ Verify NulleX block count -------------------------
+Latest online block: 135491
+Current local block: 135491
 NulleX masternode is in sync.
 
------------------- Verify NulleX installer version -------------------
+--------------- Verify NulleX balance at explorer.nullex.io ----------------
+Checking balance online for Aa1Bb2Cc3Dd4Ee5Ff6Gg7Hh8Ii9Jj0KkLl
+Previous balance: 50643
+Current balance : 50679
+NulleX rewards have been added since previous check at 2018-10-25 17:00:00.
+
+--------------------- Verify NulleX installer version ----------------------
 NulleX masternode github installer version: 1.0.6
 NulleX masternode local installed version : 1.0.6
 NulleX masternode installer version is correct.
 
--------------------- Verify NulleX wallet version --------------------
+----------------------- Verify NulleX wallet version -----------------------
 Github wallet version: 1.3.6.1
 Local wallet version : 1.3.6.1
 NulleX wallet version is correct.
 
------------------- Verify NulleX masternode status -------------------
-NulleX masternode status is OK.
-
----------------------- Verify NulleX process ID ----------------------
-PID file /home/user/.nullexqt/NulleXd.pid exists.
+------------------------- Verify NulleX process ID -------------------------
+PID file /home/userid/.nullexqt/NulleXd.pid exists.
 Process ID: 2938
 
-------------- Verify number of running NulleX processes --------------
+---------------- Verify number of running NulleX processes -----------------
 UID        PID  PPID  C STIME TTY          TIME CMD
-user      2938     1  1 Oct05 ?        06:29:50 nullexd -resync
+eddy      2938     1  1 Oct05 ?        07:23:06 nullexd -resync
 Expected number of NulleX processes running (1 of 1).
 
------------- Verify NulleX balance at explorer.nullex.io -------------
-Checking balance online for Aa1Bb2Cc3Dd4Ee5Ff6Gg7Hh8Ii9Jj0KkLl
-Previous balance: 50614
-Current balance : 50614
-NulleX balance unchanged since check at 2018-10-23 17:00:04.
-
----------------- Verify Masternode server disk space -----------------
+---------------- Verify NulleX masternode server disk space ----------------
 NulleX masternode server disk space is OK.
 
------------------------------- Summary -------------------------------
+----------------- Verify NulleX masternode port connection -----------------
+Proto Recv-Q Send-Q Local Address           Foreign Address         State
+tcp        0      0 your.domain.name:43879  *:*                     LISTEN
+NulleX masternode is listening on port 43879.
+
+NulleX masternode external IP-address: 12.34.56.78
+Connection to 12.34.56.78 43879 port [tcp/*] succeeded!
+Connection to 12.34.56.78 on port 43879 succeeded.
+
+--------------------------------- Summary ----------------------------------
+Checks  : 9
 Warnings: 0
 Errors  : 0
 
